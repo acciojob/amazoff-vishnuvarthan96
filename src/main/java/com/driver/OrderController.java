38e98564c44987f9388ec170773549316e63928d
vishnuvarthan <vishnuv9677@gmail.com>
2023-08-30 13:16:53 +0530

    check

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("orders")
+//@RequestMapping("orders")
 public class OrderController {
         @Autowired
         private OrderService orderServiceObj;
